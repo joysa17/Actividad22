3624ddd6409c32a4bd9177bc0d520fec1984c7b9
joysa <joselin.solis.3b@gmail.com>
2017-10-04 22:02:03 -0300

    Quinto commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/landing'
+
   resources :students
-get 'students/index'
+  
+  #get 'students/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'students#index'
 end
